9e120a4a1596d8086db672fff4533738f35e97b7
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-11-10 12:07:14 +0000

    add submit

--- train_tf.py
+++ train_tf.py
@@ -7,6 +7,14 @@ from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras import metrics
 from tensorflow.keras.optimizers import Adam
 from data_tf import DataGenerator
+import pandas as pd
+import os
+import numpy as np
+import tqdm
+from tensorflow.keras.models import load_model
+
+
+
 
 
 # create train datagen
@@ -82,3 +90,19 @@ history = model.fit_generator(
     epochs=epochs,
     verbose=1,
     callbacks=[checkpointer, early_stopper])
+
+submit = pd.read_csv('/data/d14122793/human_protein_atlas_image_classification/sample_submission.csv')
+model = load_model('/data/d14122793/human_protein_atlas_image_classification/InceptionResNetV2.model')
+predicted = []
+
+for name in tqdm(submit['Id']):
+    path = os.path.join('/data/d14122793/human_protein_atlas_image_classification/test/', name)
+    image = data_gen.load_image(path, (299,299,3))
+    score_predict = model.predict(image[np.newaxis])[0]
+    label_predict = np.arange(28)[score_predict>=0.5]
+    str_predict_label = ' '.join(str(l) for l in label_predict)
+    predicted.append(str_predict_label)
+
+
+submit['Predicted'] = predicted
+submit.to_csv('submission.csv', index=False)
