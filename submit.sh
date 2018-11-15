aff5ed8e001014708d41ac1cb67eaebcb6df9968
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-11-15 11:24:19 +0000

    update

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#sbatch --job-name=KQ_submit --gres=gpu:0 --mem=16000 --cpus-per-task=4 submit.sh
+#sbatch --job-name=KQ_submit --gres=gpu:0 --mem=16000 --output=output_submit.out --cpus-per-task=4 submit.sh
 
 python3 submit.py
