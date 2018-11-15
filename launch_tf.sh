a9c792c5ec7477888323b648e2ef2ad4a6e901e3
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-11-15 11:51:02 +0000

    change

--- launch_tf.sh
+++ launch_tf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#sbatch --job-name=KQ_tftrain --gres=gpu:1 --mem=65536 --cpus-per-task=4 launch_tf.sh
+#sbatch --job-name=KQ_human --gres=gpu:1 --mem=4096 --cpus-per-task=2 --nodelist=AIRC-01 launch_tf.sh
 
 python3 train_tf.py
